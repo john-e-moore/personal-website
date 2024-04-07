efddf5cbdc6143ec72e594290d65c12f7da3f7fb
john-e-moore <jmoore87jr@gmail.com>
2024-04-07 15:20:27 +0000

    wsgi app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
 from app import routes
+
+# Wrap the app in ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
+
