43c066f504f7b72a98c1804ce85d4fc76c15bc0d
john-e-moore <jmoore87jr@gmail.com>
2024-04-11 15:43:52 +0000

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,31 @@
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 
-from app import routes
+# Logging configuration
+if not app.debug:
+    # Ensure the directory for your logs exists; create it otherwise
+    log_directory = os.path.join(os.getcwd(), 'logs')
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+
+    file_handler = RotatingFileHandler(os.path.join(log_directory, 'personal-website.log'), maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'
+    ))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Personal website startup')
 
 # Wrap the app in ProxyFix
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
 
+from app import routes
